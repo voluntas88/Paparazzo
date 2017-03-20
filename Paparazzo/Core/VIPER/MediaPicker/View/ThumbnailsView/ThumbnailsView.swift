3ef0b6ee532aed9d687e5f2834ad9f4fc41ce1f3
Artem Peskishev <aopeskishev@avito.ru>
2017-03-20 15:27:36 +0300

    AI-4648: Fix code style, remove cast from NSNumber to Int

--- Paparazzo/Core/VIPER/MediaPicker/View/ThumbnailsView/ThumbnailsView.swift
+++ Paparazzo/Core/VIPER/MediaPicker/View/ThumbnailsView/ThumbnailsView.swift
@@ -237,7 +237,7 @@ final class ThumbnailsView: UIView, UICollectionViewDataSource, MediaRibbonLayou
     }
     
     func canMoveTo(_ indexPath: IndexPath) -> Bool {
-        let cameraCellVisible = Int(NSNumber(value: dataSource.cameraCellVisible))
+        let cameraCellVisible = dataSource.cameraCellVisible ? 1 : 0
         
         let lastSectionIndex = collectionView.numberOfSections - 1
         let lastItemIndex = collectionView.numberOfItems(inSection: lastSectionIndex) - cameraCellVisible
